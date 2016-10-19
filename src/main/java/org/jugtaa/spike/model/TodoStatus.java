fe5af2e4084fc3ace2fd489f2cfb020429a0f476
Mario Alexandro Santini <alexmario74@gmail.com>
2016-10-19 15:41:20 +0200

    Add serialization/deserialization JSON helpers

--- src/main/java/org/jugtaa/spike/model/TodoStatus.java
+++ src/main/java/org/jugtaa/spike/model/TodoStatus.java
@@ -1,5 +1,8 @@
 package org.jugtaa.spike.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Created by mario on 16/10/2016.
  */
@@ -7,5 +10,25 @@ public enum TodoStatus {
     CREATED,
     WORK_IN_PROGRES,
     SUSPENDED,
-    DONE
+    DONE;
+
+    @JsonCreator
+    public static TodoStatus forValue(String status) {
+        if (CREATED.name().equalsIgnoreCase(status)) {
+            return CREATED;
+        } else if (WORK_IN_PROGRES.name().equalsIgnoreCase(status)) {
+            return WORK_IN_PROGRES;
+        } else if (SUSPENDED.name().equalsIgnoreCase(status)) {
+            return SUSPENDED;
+        } else if (DONE.name().equalsIgnoreCase(status)) {
+            return DONE;
+        }
+
+        return null;
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
