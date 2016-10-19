44acee35fe9047970ac427e324e32ddd005d4443
Mario Alexandro Santini <alexmario74@gmail.com>
2016-10-19 15:47:01 +0200

    Refactor package name

--- launch-jetty-container.bat
+++ launch-jetty-container.bat
@@ -1,4 +1,4 @@
 
-REM "exec 'docker run --rm -p 8080:8080 --name todolist org.jugtaa.spike/todolist'"
+REM "exec 'docker run --rm -p 8080:8080 --name todolist org.jugtaas.spike/todolist'"
 
-docker run --rm -p 8080:8080 --name todolist org.jugtaa.spike/todolist
+docker run --rm -p 8080:8080 --name todolist org.jugtaas.spike/todolist
