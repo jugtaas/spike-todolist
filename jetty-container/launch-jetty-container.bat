f3beea5b8b56872d82acad76aceed16435e44378
Mario A. Santini <alexmario74@gmail.com>
2016-10-31 18:27:50 +0100

    Add link to db container

--- jetty-container/launch-jetty-container.bat
+++ jetty-container/launch-jetty-container.bat
@@ -1,4 +1,4 @@
 
-REM "exec 'docker run --rm -p 8080:8080 --name todolist org.jugtaas.spike/todolist'"
+REM "exec 'docker run --rm -p 8080:8080 --link todosql --name todolist org.jugtaas.spike/todolist'"
 
-docker run --rm -p 8080:8080 --name todolist org.jugtaas.spike/todolist
+docker run --rm -p 8080:8080 --link todosql --name todolist org.jugtaas.spike/todolist
