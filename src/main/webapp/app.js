24b97335b857b7ca5e525e9bd83c463f79c0e53d
Mario A. Santini <alexmario74@gmail.com>
2016-11-01 08:50:24 +0100

    Refactoring

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -2,30 +2,21 @@ function App() {
 	const appId = "#app";
 	const appDiv = $(appId);
 
-	function getActions(text) {
+	 function getActions(text) {
 		let actions = $("<ul></ul>");
 
 		let action = $("<li></li>");
-        let bt = $("<button title=\"delete todo\" class=\"less\" data-text=\"" + text+ "\">-</button>");
+        let bt = $("<button title=\"delete todo\" class=\"less\" data-text=\"" + text + "\">-</button>");
 		action.append(bt);
 
         bt.click(function (ev) {
             ev.preventDefault();
             let row = actions.parent().parent();
             row.remove();
-            $.ajax('/todo/'+(bt.data().text), {
-                'method': 'DELETE',
-                'success': function (res, st) {
-                    if (st === 'success') {
-                        alert("Done!");
-                    } else {
-                        console.dir(res);
-                    }
-                },
-                'error': function() {
-                    console.log("error");
-                    console.dir(arguments);
-                }
+            let text = $(this).data().text;
+
+            deleteTodo(text, function() {
+               alert("Done!\nTodo " + text + " has been delete.");
             });
         });
 
@@ -36,14 +27,6 @@ function App() {
 		td.append(actions);
 
 		return td
-	}
-
-	function TodoList() {
-		this.table = appDiv.find('table');
-		this.head = this.table.find('thead');
-		this.body = this.table.find('tbody');
-
-		this.bindEventHandlers();
 	}
 
     function updateTodo(todo) {
@@ -66,7 +49,129 @@ function App() {
         });
     }
 
-	TodoList.prototype.addRow = function (todo) {
+	function fetch() {
+		$.ajax('/todo', {
+			"dataType": "json",
+			"success": function(data, status) {
+				if (status === 'success') {
+				    App.TodoList.addTodoList(data.todoList);
+				}
+			},
+			"error": function() {
+				console.log("error");
+				console.dir(arguments);
+			}
+		});
+	}
+
+    function addTodo(todo) {
+        $.ajax('/todo', {
+            'method': "PUT",
+            'dataType': 'json',
+            'data': JSON.stringify(todo),
+            'contentType': 'application/json',
+            'success': function(req, stat) {
+                if (stat === 'success') {
+                    // console.dir(req);
+                } else {
+                    // alert(req);
+                    // console.log(req);
+                }
+            },
+            'error': function(err, stat) {
+                console.log(stat);
+                console.dir(err);
+            }
+        });
+    }
+
+    function fetchOne(todo, cbk) {
+        $.ajax({
+            'url': '/todo/' + todo.text,
+            'method': 'GET',
+            'success': function (req, sts) {
+                if (sts === 'success') {
+                    cbk(req);
+                } else {
+                   // something here
+                }
+            },
+            "error": function (err) {
+                console.log(err);
+            }
+        });
+    }
+
+    function deleteTodo(text, cbk) {
+        $.ajax('/todo/'+(text), {
+            'method': 'DELETE',
+            'success': function (res, st) {
+                if (st === 'success') {
+                    cbk();
+                } else {
+                   // console.dir(res);
+                }
+            },
+            'error': function() {
+                console.log("error");
+                console.dir(arguments);
+            }
+        })
+    }
+
+	function updateTodoStatus(ev) {
+	    let checkbox = $(this);
+        let _status = checkbox.data();
+        if (_status.done) {
+            checkbox.data('done', undefined);
+        } else {
+            checkbox.data('done', (new Date()).getTime());
+        }
+        let todo = {
+            'id': checkbox.data('id'),
+            'status': (checkbox.data('done') ? 'DONE' : 'CREATED'),
+            'done': checkbox.data('done')
+        };
+        checkbox.parent().next().toggleClass('tododone');
+        if (todo.id === undefined) {
+            fetchOne(todo, function(req){
+                checkbox.data('id', req.id);
+                todo.id = req.id;
+                updateTodo(todo);
+            });
+        } else {
+            updateTodo(todo);
+        }
+    }
+
+    function createTodo(text) {
+        return {status: 'CREATED',
+                created: (new Date()).getTime(),
+                text: text};
+    }
+
+    function addTodoHandler(ev) {
+        ev.preventDefault();
+        if ($('#todo_text').val() != '') {
+            let todo = createTodo($('#todo_text').val());
+            App.TodoList.addRow(todo, true);
+            $('#todo_text').val('');
+            App.TodoList.textInput.css({'top': -100, 'left': 0});
+            App.TodoList.textInput.hide();
+            addTodo(todo);
+        }
+    }
+
+	function TodoList() {
+		this.table = appDiv.find('table');
+		this.head = this.table.find('thead');
+		this.body = this.table.find('tbody');
+
+		this.bindEventHandlers();
+		setTimeout(fetch, 10);
+	}
+
+	TodoList.prototype.addRow = function(todo, before) {
 		let row = $("<tr></tr>");
 
 		let td = $("<td></td>");
@@ -89,40 +194,7 @@ function App() {
 
 				td.append(checkbox);
 
-                checkbox.click(function (ev) {
-                    let _status = checkbox.data();
-                    if (_status.done) {
-                        checkbox.data('done', undefined);
-                    } else {
-                        checkbox.data('done', (new Date()).getTime());
-                    }
-                    let todo = {
-                        'id': checkbox.data('id'),
-                        'status': (checkbox.data('done') ? 'DONE' : 'CREATED'),
-                        'done': checkbox.data('done')
-                    };
-                    checkbox.parent().next().toggleClass('tododone');
-                    if (todo.id === undefined) {
-                        $.ajax({
-                            'url': '/todo/' + checkbox.parent().next().text(),
-                            'method': 'GET',
-                            'success': function (req, sts) {
-                                if (sts === 'success') {
-                                    checkbox.data('id', req.id);
-                                    todo.id = req.id;
-                                    updateTodo(todo);
-                                } else {
-                                   // something here
-                                }
-                            },
-                            "error": function (err) {
-                                console.log(err);
-                            }
-                        });
-                    } else {
-                        updateTodo(todo);
-                    }
-                });
+                checkbox.click(updateTodoStatus);
 
 				break;
 
@@ -132,7 +204,9 @@ function App() {
 				checkbox.attr("title", "Done " + (new Date(parseInt(todo.done, 10))));
 
 				td.append(checkbox);
+
 				break;
+
 		}
 
 	    row.append(td);
@@ -149,62 +223,22 @@ function App() {
 
 	    row.append(getActions(todo.text));
 
-		this.body.append(row);
-	}
-
-	TodoList.prototype.fetch = function () {
-		$.ajax('/todo', {
-			"dataType": "json",
-			"success": function(data, status) {
-				if (status === 'success') {
-				    data.todoList.forEach(todo => {
-				        App.TodoList.addRow(todo)
-				    });
-				}
-			},
-			"error": function() {
-				console.log("error");
-				console.dir(arguments);
-			}
-		});
-	}
-
-    function createTodo (text) {
-        return {status: 'CREATED',
-                created: (new Date()).getTime(),
-                text: text};
-    }
-
-    function addTodoHandler (ev) {
-        ev.preventDefault();
-        if ($('#todo_text').val() != '') {
-            let todo = createTodo($('#todo_text').val());
-            App.TodoList.addRow(todo);
-            $('#todo_text').val('');
-            App.TodoList.textInput.css({'top': -100, 'left': 0});
-            App.TodoList.textInput.hide();
-            $.ajax('/todo', {
-                'method': "PUT",
-                'dataType': 'json',
-                'data': JSON.stringify(todo),
-                'contentType': 'application/json',
-                'success': function(req, stat) {
-                    if (stat === 'success') {
-                        console.dir(req);
-                    } else {
-                        alert(req);
-                        console.log(req);
-                    }
-                },
-                'error': function(err, stat) {
-                    console.log(stat);
-                    console.dir(err);
-                }
-            });
+        if (before) {
+            before = this.body.children().length > 0;
         }
+
+        if (!before) {
+           this.body.append(row);
+        } else {
+           row.insertBefore(this.body.children()[0]);
+        }
+	}
+
+    TodoList.prototype.addTodoList = function (todoList) {
+        todoList.forEach(App.TodoList.addRow.bind(App.TodoList));
     }
 
-    TodoList.prototype.showText = function () {
+    TodoList.prototype.showText = function() {
         if (!this.textInput) {
             this.textInput = $('span.hide');
 
@@ -222,13 +256,16 @@ function App() {
 
         this.textInput.css({'top': pos.top, 'left': pos.left});
         this.textInput.show();
+        this.textInput.find('input').focus();
+    }
+
+    TodoList.prototype.showAddText = function(ev) {
+        ev.preventDefault();
+        this.showText();
     }
 
     TodoList.prototype.bindEventHandlers = function () {
-        $('.plus').click(function (ev) {
-            ev.preventDefault();
-           App.TodoList.showText();
-        });
+        $('.plus').click(this.showAddText.bind(this));
     }
 
 	App.TodoList = new TodoList();
